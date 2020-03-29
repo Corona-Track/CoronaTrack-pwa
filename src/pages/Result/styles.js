e788d5a905f4e0b8a1dd5f5cd17a360dbc39bb50
Deise Koreeda <deise.koreeda@gmail.com>
2020-03-29 16:13:46 -0300

    fix: styles in result component

--- src/pages/Result/styles.js
+++ src/pages/Result/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { $gray600, $gray700 } from '../../styles/variables';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles(() => ({
     result : {
       paddingLeft: '2rem',
       paddingRight: '2rem'
@@ -10,23 +10,5 @@ export const useStyles = makeStyles((theme) => ({
       color: $gray600,
       marginTop: '3rem',
       fontFamily: "'Prompt', sans-serif",
-    },
-    // chronicDiseasesQuestion: {
-    //   fontFamily: "'Prompt', sans-serif",
-    //   fontWeight: 500,
-    //   fontSize: '1.3rem',
-    //   lineHeight: '2rem',
-    //   marginTop: '1rem'
-    // },
-    // formControl: {
-    //   color: $gray700,
-    //   marginTop: '2.8rem',
-    //   '& label > span:nth-child(2)': {
-    //     fontWeight: 500,
-    //     fontFamily: "'Prompt', sans-serif",
-    //   }
-    // },
-    // chronicDiseasesBtn: {
-    //   marginTop: '2rem'
-    // }
+    }
   }));
