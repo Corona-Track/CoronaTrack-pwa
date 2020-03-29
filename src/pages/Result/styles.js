2886e582f2b6c3e275dd529e0782d0da6db511fe
Deise Koreeda <deise.koreeda@gmail.com>
2020-03-29 17:19:23 -0300

    fix: indentation

--- src/pages/Result/styles.js
+++ src/pages/Result/styles.js
@@ -3,12 +3,23 @@ import { $gray600, $gray700 } from '../../styles/variables';
 
 export const useStyles = makeStyles(() => ({
     result : {
-      paddingLeft: '2rem',
-      paddingRight: '2rem'
+        paddingLeft: '2rem',
+        paddingRight: '2rem',
+        fontSize: '1rem',
     },
     resultLabel: {
-      color: $gray600,
-      marginTop: '3rem',
-      fontFamily: "'Prompt', sans-serif",
+        color: $gray600,
+        marginTop: '3rem',
+        fontFamily: "'Prompt', sans-serif",
+    },
+    resultBtn: {
+        marginTop: '2rem',
+    },
+    resultFooter: {
+        fontSize: '0.75rem',
+        marginRight: '3rem',
+        marginLeft: '3rem',
+        textAlign: 'center',
+        color: $gray700
     }
-  }));
+}));
