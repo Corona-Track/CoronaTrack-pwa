a8f9f4b152dcab72d5c78de6eab67b43fae3074f
dkoreeda <deise.koreeda@gmail.com>
2020-03-29 13:59:36 -0300

    Tela de sintomas (#8)
    Tela de sintomas (#8)

---
+++ src/pages/Symptoms/styles.js
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { $gray600, $gray700 } from '../../styles/variables';
+
+export const useStyles = makeStyles(() => ({
+    symptoms : {
+      paddingLeft: '2rem',
+      paddingRight: '2rem'
+    },
+    symptomsLabel: {
+      color: $gray600,
+      marginTop: '3rem'
+    },
+    symptomsQuestion: {
+      fontWeight: 500,
+      fontSize: '1.3rem',
+      lineHeight: '2rem',
+      marginTop: '1rem'
+    },
+    formControl: {
+      color: $gray700,
+      marginTop: '2.8rem',
+      '& label > span:nth-child(2)': {
+        fontWeight: 500,
+        fontFamily: "'Prompt', sans-serif",
+      }
+    },
+    symptomsBtn: {
+      marginTop: '2rem'
+    }
+  }));
