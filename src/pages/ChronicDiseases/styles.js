a7eb1fcce58a7cff474a069b73d14390e2d5fc7a
geovanasilva <sts.geovana@gmail.com>
2020-03-29 02:32:16 -0300

    style: 💄 add style to chronic diseases page

---
+++ src/pages/ChronicDiseases/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { $gray600, $gray700 } from '../../styles/variables';
+
+export const useStyles = makeStyles((theme) => ({
+    chronicDiseases : {
+      paddingLeft: '2rem',
+      paddingRight: '2rem'
+    },
+    chronicDiseasesLabel: {
+      color: $gray600,
+      marginTop: '3rem'
+    },
+    chronicDiseasesQuestion: {
+      fontWeight: 500,
+      fontSize: '1.3rem',
+      lineHeight: '2rem',
+      marginTop: '1rem'
+    },
+    formControl: {
+      color: $gray700,
+      marginTop: '2.8rem'
+    },
+    chronicDiseasesBtn: {
+      marginTop: '2rem'
+    }
+  }));
