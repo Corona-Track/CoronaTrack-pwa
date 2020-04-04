3b47ac6a3fe332305c145c45e1076669279ea423
jonasjs <joonasjss@gmail.com>
2020-04-04 20:57:17 -0300

    fix: 🐛 fiz url

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -48,7 +48,8 @@ export default function Home() {
         <Share active={shareActive} onClose={handleShare} />
         {coord.latitude && (
           <Iframe
-            url={`https://site58987541.westus2.cloudapp.azure.com:81/epidemia/Grafico/Map?modulo=Geociencia&acesso_publico=S&acesso_mobile=S&integracao=S&latitude=${coord.latitude}&longitude=${coord.longitude}&zoom=10&uid=${uid}`}
+            url={`https://site58987541.westus2.cloudapp.azure.com/epidemia/Grafico/Map?modulo=Geociencia&acesso_publico=S&acesso_mobile=S&integracao=S&latitude=${coord.latitude ||
+              0}&longitude=${coord.longitude || 0}&zoom=10&uid=${uid}`}
             width="100%"
             height="100%"
           />
